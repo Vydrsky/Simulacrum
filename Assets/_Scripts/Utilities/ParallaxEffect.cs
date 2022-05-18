a68b1e4ce35dcd5f2bc4475104c9edc00649988c
Vydrsky <vydrsky@gmail.com>
2022-05-18 23:00:39 +0200

    bigger level

--- Assets/_Scripts/Utilities/ParallaxEffect.cs
+++ Assets/_Scripts/Utilities/ParallaxEffect.cs
@@ -8,6 +8,7 @@ public class ParallaxEffect : MonoBehaviour {
     private Camera mainCam;
     private Transform[] childTransforms;
     private Transform furthestChild;
+    private int immediateChildren = 0;
 
 
     [Range(0f, 1f)]
@@ -17,12 +18,17 @@ public class ParallaxEffect : MonoBehaviour {
     private void Awake() {
         mainCam = Camera.main;
         startpos = transform.position.x;
-        length = GameObject.FindGameObjectWithTag("backgroundBase").GetComponent<SpriteRenderer>().bounds.size.x;
-        childTransforms = GetComponentsInChildren<Transform>();
-        furthestChild = childTransforms[1];
     }
 
     private void Start() {
+        length = GameObject.FindGameObjectWithTag("backgroundBase").GetComponent<SpriteRenderer>().bounds.size.x;
+        childTransforms = GetComponentsInChildren<Transform>();
+        furthestChild = childTransforms[1];
+        foreach (Transform child in transform) {
+            if (child == childTransforms[0]) continue;
+            //if (child.parent != gameObject) continue;
+            immediateChildren++;
+        }
     }
 
     /************************ LOOPING ************************/
@@ -30,34 +36,31 @@ public class ParallaxEffect : MonoBehaviour {
         float cameraRelativePosition = mainCam.transform.position.x * (1 - parallaxEffect);
         float distance = mainCam.transform.position.x * parallaxEffect;
         float padding = 0.9f;
+
         transform.position = new Vector3(distance, transform.position.y, transform.position.z);
-        
+        Debug.Log(immediateChildren);
         if(cameraRelativePosition > startpos + padding*length) {
-            foreach(Transform child in childTransforms) {
-                if (child == childTransforms[0]) continue;
+            foreach(Transform child in transform) {
                 //get all background children
                 if(child.position.x < furthestChild.position.x) {
                     furthestChild = child;
                 }
             }
-            //move the most left children right by amount of children tiles
-            furthestChild.position += new Vector3((childTransforms.Length-1) * length, 0f, 0f);
+            //move the most left children right by amount of immediate children tiles
+            furthestChild.position += new Vector3(immediateChildren * length, 0f, 0f);
             //set new start position for the beggining of the next tile
             startpos += padding * length;
         }
         if (cameraRelativePosition < startpos - padding * length) {
-            foreach (Transform child in childTransforms) {
-                if (child == childTransforms[0]) continue;
+            foreach (Transform child in transform) {
                 if (child.position.x > furthestChild.position.x) {
                     furthestChild = child;
                 }
             }
-            furthestChild.position += new Vector3((childTransforms.Length - 1) * -length, 0f, 0f);
+            furthestChild.position += new Vector3(immediateChildren * -length, 0f, 0f);
             startpos -= padding * length;
         }
     }
-
-    /************************ METHODS ************************/
 
 
 }
